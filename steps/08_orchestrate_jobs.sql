231b2577dfc4779016a3d7b34ab6f34157db5731
sfc-gh-praj <phani.raj@snowflake.com>
2023-05-10 12:41:39 +0530

    making minor changes

--- steps/08_orchestrate_jobs.sql
+++ steps/08_orchestrate_jobs.sql
@@ -38,7 +38,7 @@ CALL ANALYTICS.DAILY_CITY_METRICS_UPDATE_SP();
 -- ----------------------------------------------------------------------------
 
 ALTER TASK DAILY_CITY_METRICS_UPDATE_TASK RESUME;
-EXECUTE TASK ORDERS_UPDATE_TASK;
+-- EXECUTE TASK ORDERS_UPDATE_TASK;
 
 
 -- ----------------------------------------------------------------------------
